3836eb1b4b0b51fdb74332679136731b6910f550
1Shyy <wwe.sobolev@yandex.ru>
2023-10-03 22:00:28 +0300

    with some scripts

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,37 @@ document.addEventListener('DOMContentLoaded', clickTabs());
                         }
                     });
                 }
+
+                window.addEventListener('scroll', () => {
+                    const verticalScrollPx = window.scrollY || window.pageYOffset;
+                    const page = document.querySelector('.page');
+                    let changeColor = document.querySelectorAll('.page');
+                  
+                    if (verticalScrollPx < 500) {
+                      document.getElementById('page1').style.color = '#FF3E41';
+                      document.getElementById('page2').style.color = '#7F8589';
+                    } else if (verticalScrollPx > 700 && verticalScrollPx < 3000) {
+                        document.getElementById('page3').style.color = '#7F8589';
+                        document.getElementById('page2').style.color = '#FF3E41';
+                        document.getElementById('page1').style.color = '#7F8589';
+                    } else if (verticalScrollPx > 3800 && verticalScrollPx < 4800) {
+                        document.getElementById('page1').style.color = '#7F8589';
+                        document.getElementById('page4').style.color = '#7F8589';
+                        document.getElementById('page3').style.color = '#FF3E41';
+                        document.getElementById('page2').style.color = '#7F8589';
+                    } else if (verticalScrollPx > 4800 && verticalScrollPx < 5750) {
+                        document.getElementById('page1').style.color = '#7F8589';
+                        document.getElementById('page5').style.color = '#7F8589';
+                        document.getElementById('page4').style.color = '#FF3E41';
+                        document.getElementById('page3').style.color = '#7F8589';
+                    } else if (verticalScrollPx > 5750 && verticalScrollPx < 6400) {
+                        document.getElementById('page1').style.color = '#7F8589';
+                        document.getElementById('page6').style.color = '#7F8589';
+                        document.getElementById('page5').style.color = '#FF3E41';
+                        document.getElementById('page4').style.color = '#7F8589';
+                    } else if (verticalScrollPx > 6400){
+                        document.getElementById('page1').style.color = '#7F8589';
+                        document.getElementById('page6').style.color = '#FF3E41';
+                        document.getElementById('page5').style.color = '#7F8589';
+                    }
+                  });
